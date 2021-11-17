2677cc16341bc2150fc2e35e3757c638633b08d4
Anton Ukhanev <xedin.unknown@gmail.com>
2021-11-17 08:44:22 +0100

    Correct class docs and remove unused imports

--- src/PipeInterface.php
+++ src/PipeInterface.php
@@ -5,12 +5,11 @@ declare(strict_types=1);
 namespace Dhii\Pipe;
 
 use Dhii\Pipe\Exception\DispatchExceptionInterface;
-use Dhii\Pipe\Exception\MiddlewareExceptionInterface;
 use Dhii\Pipe\Exception\PipeExceptionInterface;
 
 /**
- * @template I
- * @template O
+ * @template Input
+ * @template Output
  */
 interface PipeInterface
 {
@@ -20,9 +19,9 @@ interface PipeInterface
      * This will start a sequence of middlewares, which process the input.
      *
      * @param mixed $input The input to process by the pipe.
-     * @psalm-param I $input
+     * @psalm-param \Input $input
      * @return mixed The result of processing.
-     * @psalm-return O
+     * @psalm-return \Output
      * @throws DispatchExceptionInterface If one of the middlewares caused an error.
      * @throws PipeExceptionInterface If problem dispatching.
      */

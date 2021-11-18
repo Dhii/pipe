394c1957905146a61af40c439d335e9badc0680b
Anton Ukhanev <xedin.unknown@gmail.com>
2021-11-18 07:00:53 +0100

    Add `MiddlewareException`

---
+++ src/Exception/MiddlewareException.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dhii\Pipe\Exception;
+
+use Dhii\Pipe\MiddlewareInterface;
+use RuntimeException;
+use Throwable;
+
+/**
+ * A problem with a middleware.
+ */
+class MiddlewareException extends RuntimeException implements MiddlewareExceptionInterface
+{
+    /**
+     * @var MiddlewareInterface
+     */
+    protected $middleware;
+
+    public function __construct(
+        $message,
+        MiddlewareInterface $middleware,
+        Throwable $previous = null,
+        $code = 0
+    ) {
+        parent::__construct($message, $code, $previous);
+        $this->middleware = $middleware;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getMiddleware(): MiddlewareInterface
+    {
+        return $this->middleware;
+    }
+}
